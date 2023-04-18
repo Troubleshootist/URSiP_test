7a649e539eb90d04a51c5a0784e97cee09986a61
troubleshootist <turboshnek@yandex.ru>
2023-04-18 17:53:56 +0300

    parser.py refactored again. parce_cell method removed. added period and data_type parsing

--- parser.py
+++ parser.py
@@ -1,13 +1,14 @@
+from pprint import pprint
+
 from openpyxl import load_workbook
 from openpyxl.cell import Cell
 from models import *
 
-DATA_TYPE_QLIQ = 'Qliq'
-DATA_TYPE_QOIL = 'Qoil'
-
 
 class XlsxParser:
     data_rows = []
+    data_types = {}
+    periods = {}
 
     def __init__(self, file_path):
         self.file = file_path
@@ -15,23 +16,29 @@ class XlsxParser:
     def parse_xlsx(self):
         wb = load_workbook(self.file)
         ws = wb.active
+        self.__parse_data_types(ws)
+        self.__parse_periods(ws)
         for row in ws.iter_rows(min_row=4):
             company = Company(row[1].value)
             for cell in row:
-                if 1 < cell.col_idx < 6:
-                    self.__parse_cell(Period('fact'), cell, company, row)
-                else:
-                    self.__parse_cell(Period('forecast'), cell, company, row)
+                if 2 < cell.col_idx < 11 and cell.col_idx % 2 != 0:
+                    self.__append_data_row(period=self.periods[cell.col_idx],
+                                           company=company,
+                                           data_type=self.data_types[cell.col_idx],
+                                           cell=cell,
+                                           row=row)
 
-    def __parse_cell(self, period: Period, cell: Cell, company: Company, row: tuple):
-        if cell.col_idx == 3:
-            self.__append_data_row(period, company, DataType(DATA_TYPE_QLIQ), cell, row)
-        elif cell.col_idx == 5:
-            self.__append_data_row(period, company, DataType(DATA_TYPE_QOIL), cell, row)
-        elif cell.col_idx == 7:
-            self.__append_data_row(period, company, DataType(DATA_TYPE_QLIQ), cell, row)
-        elif cell.col_idx == 9:
-            self.__append_data_row(period, company, DataType(DATA_TYPE_QOIL), cell, row)
+    def __parse_data_types(self, ws):
+        for cell in ws[2]:
+            if cell.value:
+                self.data_types[cell.col_idx] = DataType(cell.value)
+                self.data_types[cell.col_idx + 1] = DataType(cell.value)
+
+    def __parse_periods(self, ws):
+        for cell in ws[1]:
+            if cell.value and cell.col_idx > 2:
+                for index in range(cell.col_idx, cell.col_idx + 4):
+                    self.periods[index] = Period(cell.value)
 
     def __append_data_row(self, period: Period, company: Company, data_type: DataType, cell: Cell, row: tuple):
         self.data_rows.append(DataRow(
